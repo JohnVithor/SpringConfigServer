1c6d0973c26a14b9844fc1d47983b4ed68e01640
JohnVithor <jv.venceslau.c@gmail.com>
2022-12-08 18:47:05 -0300

    atualiza configurações do bulkhead

--- src/main/java/urfn/imd/jv/springconfigserver/SpringConfigServerApplication.java
+++ src/main/java/urfn/imd/jv/springconfigserver/SpringConfigServerApplication.java
@@ -1,13 +1,20 @@
 package urfn.imd.jv.springconfigserver;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
 
 @EnableConfigServer
 @SpringBootApplication
 public class SpringConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringConfigServerApplication.class, args);
+    }
+
+    @Bean
+    public Sampler alwaysSampler() {
+        return Sampler.ALWAYS_SAMPLE;
     }
 }
